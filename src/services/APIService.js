34f6ac93fa7f7220aa002694bb38b7626de6f5cf
Michelle Primiani <primianimichelle@gmail.com>
2023-09-19 12:29:45 -0400

    Student can register for events

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -30,3 +30,18 @@ export const deleteEvent = async (eventToDelete) => {
   const deletedEvent = await response.json();
   return deletedEvent;
 };
+
+export const deleteRegistration = async (registrationIdToDelete) => {
+  const response = await fetch(
+    `http://localhost:8088/registrations/${registrationIdToDelete}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const deletedEvent = await response.json();
+  return deletedEvent;
+};
